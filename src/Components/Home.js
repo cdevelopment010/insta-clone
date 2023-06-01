a679bda55aa6a62673aec9ec730922d8a623a8bb
Craig <cdevelopment010@gmail.com>
2023-06-01 21:06:44 +0100

    Feat: Icons for menu

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,13 @@
 
 import Avatar from "./Avatar"
+import Menu from "./Menu"
 
 export default function Home() {
 
     return (
         <div>
             <Avatar size="xl" bg="bg" src="https://static-cdn.jtvnw.net/jtv_user_pictures/5e73945e-893e-4722-87fe-14727087542c-profile_image-70x70.png"/>
+            <Menu />
         </div>
     )
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,4 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faHouse, faMagnifyingGlass, faBars  } from "@fortawesome/free-solid-svg-icons"
+import { faCompass, faPaperPlane, faHeart, faPlusSquare  } from "@fortawesome/fontawesome-free-regular";
 
+
+import '../Styles/menu.css';
 
 export default function Menu() {
 
@@ -6,6 +11,17 @@ export default function Menu() {
     return (
         <div>
             <span>Logo desktop...</span>
+            <div className="d-flex align-items-center justify-content-evenly w-50">
+                <FontAwesomeIcon icon= {faHouse} />
+                <FontAwesomeIcon icon= {faMagnifyingGlass} />
+                <FontAwesomeIcon icon= {faCompass} />
+                <img className="reels" src={process.env.PUBLIC_URL + '/images/instagram-reels-icon.svg'} alt="reels" />
+                <FontAwesomeIcon icon= {faPaperPlane} />
+                <FontAwesomeIcon icon= {faHeart} />
+                <FontAwesomeIcon icon= {faPlusSquare} />
+                <FontAwesomeIcon icon= {faBars} />
+            </div>
+
         </div>
     )
 
