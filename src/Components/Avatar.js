2e1aa6f896ef85cd4894c6f4ae0837d979deba9c
Craig <cdevelopment010@gmail.com>
2023-06-04 21:18:41 +0100

    Feat: replace fontawesome menu icons with svgs

--- src/Components/Avatar.js
+++ src/Components/Avatar.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from "react"
 import '../Styles/avatar.css';
 
-export default function Avatar({size =null, bg = null, src = null}) {
+export default function Avatar({size =null, bg = null, src = null, className = ""}) {
 
     useEffect(() => {
 
     }) 
     return (
         <div>
-            <div className={`profile-pic ${size ? size : ''} ${bg ? bg : ''}`}>
+            <div className={`profile-pic ${size ? size : ''} ${bg ? bg : ''} ${className}`}>
                 <img src={src ? src : ''} />
             </div>    
         </div>
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHouse as faHouseSolid, faMagnifyingGlass, faBars, faCompass as faCompassSolid  } from "@fortawesome/free-solid-svg-icons"
-import { faCompass, faPaperPlane, faHeart, faPlusSquare  } from "@fortawesome/fontawesome-free-regular";
+
+import Avatar from './Avatar';
 
 
 import '../Styles/menu.css';
@@ -17,14 +18,15 @@ export default function Menu() {
             <div>
                 <div className="mb-5 pb-5">Logo desktop...</div>
                 <div className="d-flex align-items-start justify-content-evenly flex-column menu-items-container">
-                    <ul>
-                        <li><FontAwesomeIcon icon= {faHouseSolid} className="menu-item" /></li>
-                        <li><FontAwesomeIcon icon= {faMagnifyingGlass}  className="menu-item" /></li>
-                        <li><FontAwesomeIcon icon= {faCompass}  className="menu-item compass" onFocus={changeToSolid}/></li>
-                        <li><img className="reels menu-item" src={process.env.PUBLIC_URL + '/images/instagram-reels-icon.svg'} alt="reels" /></li>
-                        <li><FontAwesomeIcon icon= {faPaperPlane}  className="menu-item"/></li>
-                        <li><FontAwesomeIcon icon= {faHeart}  className="menu-item"/></li>
-                        <li><FontAwesomeIcon icon= {faPlusSquare}  className="menu-item"/></li>
+                    <ul className="d-flex align-items-start justify-content-evenly flex-column menu-list">
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/home-solid.svg" alt="" className="menu-item me-2" /> Home</li>
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/search-regular.svg" alt="" className="menu-item me-2" />Search</li>
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/compass-north.svg" alt="" className="menu-item me-2 compass" />Explore</li>
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/instagram-reels-regular.svg" alt="" className="menu-item me-2" />Reels</li>
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/instagram-share-regular.svg" alt="" className="menu-item me-2" />Messages</li>
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/heart-regular.svg" alt="" className="menu-item me-2" />Notifications</li>
+                        <li className="d-flex align-items-center"><img src="/Icons/svg/add-button-regular.svg" alt="" className="menu-item me-2" />Create</li>
+                        <li className="d-flex align-items-center"><Avatar className="menu-item me-2" />Profile</li>
 
                     </ul>
                 </div>
