4c01025ae6f4807b13ba3750ba4f2057104ac4ee
Craig <cdevelopment010@gmail.com>
2023-06-04 12:23:32 +0100

    Feat: png icons to use instead of fontawesome

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faHouse, faMagnifyingGlass, faBars  } from "@fortawesome/free-solid-svg-icons"
+import { faHouse as faHouseSolid, faMagnifyingGlass, faBars, faCompass as faCompassSolid  } from "@fortawesome/free-solid-svg-icons"
 import { faCompass, faPaperPlane, faHeart, faPlusSquare  } from "@fortawesome/fontawesome-free-regular";
 
 
@@ -7,19 +7,26 @@ import '../Styles/menu.css';
 
 export default function Menu() {
 
+    function changeToSolid(e) {
+        console.log(e);
+    }
+
     //desktop version
     return (
         <div className="menu-container d-flex flex-column justify-content-between align-items-start p-5">
             <div>
                 <div className="mb-5 pb-5">Logo desktop...</div>
                 <div className="d-flex align-items-start justify-content-evenly flex-column menu-items-container">
-                    <FontAwesomeIcon icon= {faHouse} className="menu-item"/>
-                    <FontAwesomeIcon icon= {faMagnifyingGlass}  className="menu-item"/>
-                    <FontAwesomeIcon icon= {faCompass}  className="menu-item"/>
-                    <img className="reels menu-item" src={process.env.PUBLIC_URL + '/images/instagram-reels-icon.svg'} alt="reels" />
-                    <FontAwesomeIcon icon= {faPaperPlane}  className="menu-item"/>
-                    <FontAwesomeIcon icon= {faHeart}  className="menu-item"/>
-                    <FontAwesomeIcon icon= {faPlusSquare}  className="menu-item"/>
+                    <ul>
+                        <li><FontAwesomeIcon icon= {faHouseSolid} className="menu-item" /></li>
+                        <li><FontAwesomeIcon icon= {faMagnifyingGlass}  className="menu-item" /></li>
+                        <li><FontAwesomeIcon icon= {faCompass}  className="menu-item compass" onFocus={changeToSolid}/></li>
+                        <li><img className="reels menu-item" src={process.env.PUBLIC_URL + '/images/instagram-reels-icon.svg'} alt="reels" /></li>
+                        <li><FontAwesomeIcon icon= {faPaperPlane}  className="menu-item"/></li>
+                        <li><FontAwesomeIcon icon= {faHeart}  className="menu-item"/></li>
+                        <li><FontAwesomeIcon icon= {faPlusSquare}  className="menu-item"/></li>
+
+                    </ul>
                 </div>
             </div>
             <div>
